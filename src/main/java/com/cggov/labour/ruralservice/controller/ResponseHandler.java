5c7d2813e2bac426e05934c9849d6b15c8586608
gopeshnxg <95244503+gopeshnxg@users.noreply.github.com>
2022-11-09 14:57:15 -0500

    response handler fix

--- src/main/java/com/cggov/labour/ruralservice/controller/ResponseHandler.java
+++ src/main/java/com/cggov/labour/ruralservice/controller/ResponseHandler.java
@@ -3,6 +3,7 @@ package com.cggov.labour.ruralservice.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.cggov.labour.ruralservice.api.model.ApplicantFamily;
 import com.cggov.labour.ruralservice.api.model.ApplicantInformation;
 
 import java.util.HashMap;
@@ -12,10 +13,17 @@ public class ResponseHandler {
 	public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Object responseObj) {
 		Map<String, Object> map = new HashMap<String, Object>();
 
-		if (responseObj != null) {
+		if (responseObj != null && (responseObj instanceof ApplicantInformation)) {
 			map.put("message", message);
 			map.put("status", status.value());
 			map.put("applicantId", ((ApplicantInformation) responseObj).getApplicantId());
+
+			return new ResponseEntity<Object>(map, status);
+		}
+		if (responseObj != null && (responseObj instanceof ApplicantFamily)) {
+			map.put("message", message);
+			map.put("status", status.value());
+			map.put("members created or updated: ", ((ApplicantFamily) responseObj).getMembers().size());
 
 			return new ResponseEntity<Object>(map, status);
 		}
